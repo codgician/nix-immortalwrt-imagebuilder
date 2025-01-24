275035f88e3b3994f61a7686ca25220569c55172
codgician <15964984+codgician@users.noreply.github.com>
2025-01-24 22:24:53 +0800

    generate-all-hashes: add 24.10-rc4, remove rc1 and rc2

--- generate-all-hashes.nix
+++ generate-all-hashes.nix
@@ -12,7 +12,7 @@ pkgs.writeShellApplication {
   text = ''
     VERSIONS=$(curl -s https://downloads.immortalwrt.org/.versions.json)
     readarray -t RELEASES < <(jq -r '.versions_list[]' <<< "''${VERSIONS}")
-    RELEASES+=("24.10.0-rc1" "24.10.0-rc2" "24.10.0-rc3" "24.10-SNAPSHOT")
+    RELEASES+=("24.10.0-rc3" "24.10.0-rc4" "24.10-SNAPSHOT")
 
     for RELEASE in "''${RELEASES[@]}"; do
       echo "# Fetching hashes for OpenWrt ''${RELEASE}"
